75480854a5891eeaf32156db260d74f2f55411a3
CJ Trowbridge <chris.j.trowbridge@gmail.com>
2020-06-24 00:51:26 -0700

    Update grapher.php

--- grapher.php
+++ grapher.php
@@ -10,6 +10,162 @@ function CSV($File){
 	return $csv;
 }
 
+$Colors = array(
+  'Black',
+  'Navy',
+  'DarkBlue',
+  'MediumBlue',
+  'Blue',
+  'DarkGreen',
+  'Green',
+  'Teal',
+  'DarkCyan',
+  'Color Picker',
+  'DeepSkyBlue',
+  'DarkTurquoise',
+  'MediumSpringGreen',
+  'Lime',
+  'SpringGreen',
+  'Aqua',
+  'Cyan',
+  'MidnightBlue',
+  'DodgerBlue',
+  'LightSeaGreen',
+  'ForestGreen',
+  'SeaGreen',
+  'DarkSlateGray',
+  'DarkSlateGrey',
+  'LimeGreen',
+  'MediumSeaGreen',
+  'Turquoise',
+  'RoyalBlue',
+  'SteelBlue',
+  'DarkSlateBlue',
+  'MediumTurquoise',
+  'Indigo',
+  'DarkOliveGreen',
+  'CadetBlue',
+  'CornflowerBlue',
+  'RebeccaPurple',
+  'MediumAquaMarine',
+  'DimGray',
+  'DimGrey',
+  'SlateBlue',
+  'OliveDrab',
+  'SlateGray',
+  'SlateGrey',
+  'LightSlateGray',
+  'LightSlateGrey',
+  'MediumSlateBlue',
+  'LawnGreen',
+  'Chartreuse',
+  'Aquamarine',
+  'Maroon',
+  'Purple',
+  'Olive',
+  'Gray',
+  'Grey',
+  'SkyBlue',
+  'LightSkyBlue',
+  'BlueViolet',
+  'DarkRed',
+  'DarkMagenta',
+  'SaddleBrown',
+  'DarkSeaGreen',
+  'LightGreen',
+  'MediumPurple',
+  'DarkViolet',
+  'PaleGreen',
+  'DarkOrchid',
+  'YellowGreen',
+  'Sienna',
+  'Brown',
+  'DarkGray',
+  'DarkGrey',
+  'LightBlue',
+  'GreenYellow',
+  'PaleTurquoise',
+  'LightSteelBlue',
+  'PowderBlue',
+  'FireBrick',
+  'DarkGoldenRod',
+  'MediumOrchid',
+  'RosyBrown',
+  'DarkKhaki',
+  'Silver',
+  'MediumVioletRed',
+  'IndianRed',
+  'Peru',
+  'Chocolate',
+  'Tan',
+  'LightGray',
+  'LightGrey',
+  'Thistle',
+  'Orchid',
+  'GoldenRod',
+  'PaleVioletRed',
+  'Crimson',
+  'Gainsboro',
+  'Plum',
+  'BurlyWood',
+  'LightCyan',
+  'Lavender',
+  'DarkSalmon',
+  'Violet',
+  'PaleGoldenRod',
+  'LightCoral',
+  'Khaki',
+  'AliceBlue',
+  'HoneyDew',
+  'Azure',
+  'SandyBrown',
+  'Wheat',
+  'Beige',
+  'WhiteSmoke',
+  'MintCream',
+  'GhostWhite',
+  'Salmon',
+  'AntiqueWhite',
+  'Linen',
+  'LightGoldenRodYellow',
+  'OldLace',
+  'Red',
+  'Fuchsia',
+  'Magenta',
+  'DeepPink',
+  'OrangeRed',
+  'Tomato',
+  'HotPink',
+  'Coral',
+  'DarkOrange',
+  'LightSalmon',
+  'Orange',
+  'LightPink',
+  'Pink',
+  'Gold',
+  'PeachPuff',
+  'NavajoWhite',
+  'Moccasin',
+  'Bisque',
+  'MistyRose',
+  'BlanchedAlmond',
+  'PapayaWhip',
+  'LavenderBlush',
+  'SeaShell',
+  'Cornsilk',
+  'LemonChiffon',
+  'FloralWhite',
+  'Snow',
+  'Yellow',
+  'LightYellow',
+  'Ivory'
+);
+function getColor(){
+  $Range = count($Colors) - 1;
+  $Pick = rand(0, $Range);
+  return $Colors[$Pick];
+}
+
 function findCSVs(){
   $Files = glob("./*.csv");
   return $Files;
@@ -62,6 +218,13 @@ function ArrTabler($arr, $table_class = 'table tablesorter tablesorter-ice table
   return $return;
 }
 
+function getColumn($CSV, $Index){
+  $Output = array();
+  foreach($CSV as $Row){
+    $Output[] = $Row[$Index];
+  }
+  return $Output;
+}
 
 ?>
 <!DOCTYPE html>
@@ -88,13 +251,67 @@ function ArrTabler($arr, $table_class = 'table tablesorter tablesorter-ice table
 <div class="container">
   <div class="row">
     <div class="col-12">
+      <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li class="nav-item">
+          <a class="nav-link active" id="linear-tab" data-toggle="pill" href="#linear" role="tab" aria-controls="linear" aria-selected="true">Linear Scale</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" id="logarithmic-tab" data-toggle="pill" href="#logarithmic" role="tab" aria-controls="logarithmic" aria-selected="false">Logarithmic Scale</a>
+        </li>
+      </ul>
+      <div class="tab-content" id="tabContent">
+        <div class="tab-pane fade show active" id="linear" role="tabpanel" aria-labelledby="linear-tab">
+          <canvas id="linearGraph" class="mb-2"></canvas>
+        </div>
+        <div class="tab-pane fade" id="logarithmic" role="tabpanel" aria-labelledby="logarithmic-tab">
+          <canvas id="logarithmicGraph" class="mb-2"></canvas>
+        </div>
+      </div>
+		</div>
+		<div class="col-12">
 <?php
 //Ok Begin
 
 $CSVs = findCSVs();
+
 foreach($CSVs as $CSV){
   $CSV = CSV($CSV);
   echo ArrTabler($CSV);
+  
+  $Data = array(
+    'dates'   => array(),
+    'dataset' => array()
+  ));
+  
+  
+  
+  $Index = 0;
+  $Headers = array();
+  foreach($CSV as $Rows){
+    if($Index==0){
+      foreach($Row as $Key => $Value){
+        $Headers[]=$Key;
+        $Data['dataset'][] = array(
+          'label'       => $Key,
+          'fill'        => 'false',
+          'borderColor' => getColor(),
+          'data'        => getColumn($CSV,(count($Headers[])-1))
+        );
+      }
+    }
+    
+    $Index++;
+  }
+  
+  $JSON = json_encode($Data);
+  
+  ?>
+    <script>
+      window.data = <?php echo $JSON; ?>;
+    </script>
+  <?php
+  //Later it will make tabs for more files
+  break;
 }
 
 ?>
@@ -102,7 +319,60 @@ foreach($CSVs as $CSV){
   </div><!--End row-->
 </div><!--End Container-->
 
-
+<script>
+    var linearGraph = new Chart(document.getElementById("linearGraph").getContext("2d"), {
+    type: "line",
+    data: {
+      labels: window.data.dates,
+      datasets: window.data.dataset
+    },
+    options: {
+      animation: {
+        duration: 0 // general animation time
+      },
+      hover: {
+          animationDuration: 0 // duration of animations when hovering an item
+      },
+      responsiveAnimationDuration: 0, // animation duration after a resize
+      responsive: true,
+      scales: {
+        yAxes: [{
+          type: "linear",
+          display: true
+        }]
+      },
+      legend: {
+          display: false
+      }
+    }
+  });
+  var logarithmicGraph = new Chart(document.getElementById("logarithmicGraph").getContext("2d"), {
+    type: "line",
+    data: {
+      labels: window.data.dates,
+      datasets: window.data.dataset
+    },
+    options: {
+      animation: {
+        duration: 0 // general animation time
+      },
+      hover: {
+          animationDuration: 0 // duration of animations when hovering an item
+      },
+      responsiveAnimationDuration: 0, // animation duration after a resize
+      responsive: true,
+      scales: {
+        yAxes: [{
+          type: "logarithmic",
+          display: true
+        }]
+      },
+      legend: {
+          display: false
+      }
+    }
+  });
+</script>
 
 </body>
 </html>
